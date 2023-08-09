c947ffc61d37e3e27c470e4e241c9b419cb827f3
Rakejogers <87666498+Rakejogers@users.noreply.github.com>
2023-08-09 13:06:00 -0500

    add dark/light mode toggle

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
+
+    var r = document.querySelector(':root');
+
+    function myFunction_get(variable) {
+        // Get the styles (properties and values) for the root
+        var rs = getComputedStyle(r);
+        // Alert the value of the --blue variable
+        return rs.getPropertyValue(variable);
+    }
+
+    function myFunction_set(variable, set) {
+        // Set the value of variable --blue to another value (in this case "lightblue")
+        r.style.setProperty(variable, set);
+    }
+
+    $(".switch").on("mouseup", function () {
+        if (myFunction_get("--choice")=="dark") {
+            myFunction_set("--choice", "light");
+            myFunction_set("--accent", "#FEA1A1")
+            myFunction_set("--dark", "#ECCDB4")
+            myFunction_set("--mid", "#F0EDD4")
+            myFunction_set("--light", "#F9FBE7")
+        } else {
+            myFunction_set("--choice", "dark")
+            myFunction_set("--accent", "#EC625F")
+            myFunction_set("--dark", "#313131")
+            myFunction_set("--mid", "#414141")
+            myFunction_set("--light", "#7B7B7B")
+        }
+    });
+
     $(window).scroll(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
